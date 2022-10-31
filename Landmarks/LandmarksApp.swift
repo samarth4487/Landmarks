34eb6755a0d5ebd7db09f98c43a988e9343bf59a
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2022-10-31 12:47:56 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Samarth Paboowal on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
